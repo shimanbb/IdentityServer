fb5a4f7856c6ffa19898a17355e3edd9d782730c
Debashish Paul <shimanbb@gmail.com>
2019-11-19 16:26:11 +1100

    Added Mvc Client

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -4,6 +4,7 @@
 
 using System.Collections.Generic;
 
+using IdentityServer4;
 using IdentityServer4.Models;
 
 namespace IdentityServer
@@ -11,9 +12,10 @@ namespace IdentityServer
     public static class Config
     {
         public static IEnumerable<IdentityResource> Ids =>
-            new IdentityResource[]
+            new List<IdentityResource>
             {
-                new IdentityResources.OpenId()
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
             };
 
         public static IEnumerable<ApiResource> Apis =>
@@ -40,6 +42,28 @@ namespace IdentityServer
 
                     // scopes that client has access to
                     AllowedScopes = { "api1" }
+                },
+                 // interactive ASP.NET Core MVC client
+                new Client
+                {
+                    ClientId = "mvc",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireConsent = false,
+                    RequirePkce = true,
+
+                    // where to redirect to after login
+                    RedirectUris = { "http://localhost:5002/signin-oidc" },
+
+                    // where to redirect to after logout
+                    PostLogoutRedirectUris = { "http://localhost:5002/signout-callback-oidc" },
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
                 }
             };
 
